c5869d811e29a607945b01aba08757e1402c29a6
Arif Sultan <marifsultan09@gmail.com>
2019-09-25 17:23:31 +0500

    html pages added

--- GIT TEST PROJECT/OMS/Sales_Tracker/models.py
+++ GIT TEST PROJECT/OMS/Sales_Tracker/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Ticket(models.Model):
     Pax_Name        =   models.CharField(max_length=50)
-    F_H_Name = models.CharField(max_length=50)
+    F_H_Name        =   models.CharField(max_length=50)
     Company_Name    =   models.CharField(max_length=50)
     Route           =   models.CharField(max_length=50)
     price           =   models.DecimalField(max_digits=8, decimal_places=2)
-    profit_percent   =   models.DecimalField(max_digits=3, decimal_places=1)
-    myshare = models.DecimalField(max_digits=8, decimal_places=2)
+    profit_percent  =   models.DecimalField(max_digits=3, decimal_places=1)
+    myshare         =   models.DecimalField(max_digits=8, decimal_places=2)
     Travel_agency   =   models.CharField(max_length=50)
     summary         =   models.TextField(default='This is Cool!')
 
